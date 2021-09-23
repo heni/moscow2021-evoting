15c000e4784b1c04e9b99a4475efd035f5e31e5b
Eugene Krokhalev <ekrokhalev@snapchat.com>
2021-09-23 07:14:06 +0100

    add script for database dump decoding

--- decode.py
+++ decode.py
@@ -2,11 +2,11 @@ import binascii
 import choices_pb2
 import nacl.utils, nacl.public
 
-PRIVATE_KEY = 0x54e3cf70f712b2ff727bde3849772fa811a9d5de796aa7d788d205aa86af04ad
-SECRET_KEY_OBJ = nacl.public.PrivateKey(binascii.a2b_hex(hex(PRIVATE_KEY)[2:]))
+def get_secret_object(private_key):
+    return nacl.public.PrivateKey(binascii.a2b_hex(private_key))
 
 
-def decode_choice(msg, nonce, pub_key, sKey=SECRET_KEY_OBJ):
+def decode_choice(msg, nonce, pub_key, sKey):
     pKey = nacl.public.PublicKey(binascii.a2b_hex(pub_key))
     box = nacl.public.Box(sKey, pKey)
     pbData = box.decrypt(binascii.a2b_hex(msg), binascii.a2b_hex(nonce))
---
+++ dump_votes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import dataclasses
+import datetime
+import json
+import logging
+import progressbar
+import psycopg2.extras
+import typing as tp
+
+from decode import get_secret_object, decode_choice
+
+
+@dataclasses.dataclass
+class VoteTransaction:
+    hash: str
+    datetime: datetime.datetime
+    district_id: int
+    decrypted_choice: int
+    decoded_choice: str
+
+    def as_json(self) -> dict:
+        return {"hash": self.hash, "datatime": self.datetime.strftime("%Y%m%d-%H:%M:%S"), "district_id": self.district_id, "decrypted_choice": self.decrypted_choice, "decoded_choice": self.decoded_choice}
+
+
+def get_private_key(connection: psycopg2.extensions.connection) -> str:
+    with connection.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+        cur.execute("select hash, payload from public.transactions where method_id = 8")
+        return next(iter(cur))['payload']['private_key']
+
+
+def get_vote_description(connection: psycopg2.extensions.connection) -> dict:
+    with connection.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+        cur.execute("select hash, payload from transactions where method_id = 0")
+        return next(iter(cur))['payload']
+
+
+def get_decoded_voices(connection: psycopg2.extensions.connection) -> tp.Iterable[VoteTransaction]:
+    private_key = get_private_key(connection)
+    vote_description = get_vote_description(connection)
+    sKey = get_secret_object(private_key)
+    with connection.cursor(cursor_factory = psycopg2.extras.DictCursor) as cur:
+        cur.execute("select hash, datetime, payload from public.transactions where method_id = 6 order by datetime")
+        for rec in cur:
+            enc = rec['payload']['encrypted_choice']
+            try:
+                district_id = rec['payload']['district_id']
+                decrypted_choice = decode_choice(enc['encrypted_message'], enc['nonce'], enc['public_key'], sKey)[0]
+                decoded_choice = decode_choice_option(vote_description, district_id, decrypted_choice)
+                yield VoteTransaction(rec['hash'], rec['datetime'], district_id, decrypted_choice, decoded_choice)
+            except Exception as e:
+                logging.warning(f"can't decrypt transaction with hash = {rec['hash']}: {e}")
+
+
+def decode_choice_option(vote_description, district_id, choice):
+    ballot_idx = district_id - vote_description['ballots_config'][0]['district_id']
+    district_description = vote_description['ballots_config'][ballot_idx]
+    assert district_description['district_id'] == district_id
+    return district_description['options'][str(choice)]
+
+
+if __name__ == "__main__":
+    db_name = "moscow2021"
+    db_user = 'postgres'
+    db_password = 'wee5ahLae5Ut'
+    connection = psycopg2.connect(host="127.0.0.1", port=5432, database="moscow2021", password="wee5ahLae5Ut", user="postgres")
+    with open("votes-dump.json", "w") as votes_printer:
+        for vote in progressbar.ProgressBar()(get_decoded_voices(connection)):
+            print(json.dumps(vote.as_json()), file=votes_printer)
